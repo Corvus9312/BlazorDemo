22d8d79025cf1b1c78919c5ab36675003fcaf9f2
Corvus <Corvus9312@gmail.com>
2023-10-17 16:29:36 +0800

    add-blog-post-footer

--- BlazorDemo/Pages/BlogPost.razor.cs
+++ BlazorDemo/Pages/BlogPost.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using static BlazorDemo.Pages.BlogPostFooter;
 
 namespace BlazorDemo.Pages;
 
@@ -7,15 +8,32 @@ public class BlogPostBase : ComponentBase
     [Parameter]
     public int Id { get; set; }
 
+    public FooterModel? Previous { get; set; }
+
+    public FooterModel? Next { get; set; }
+
     public string Content { get; set; } = null!;
 
-    protected override void OnInitialized()
+    protected override void OnParametersSet()
     {
+        if (Id.Equals(1))
+        {
+            Previous = null;
+            Next = new FooterModel { Title = "簡介", Url = "blog/2" };
+        }
+        else if (Id.Equals(2))
+        {
+            Previous = new FooterModel { Title = "前言", Url = "blog/1" };
+            Next = null;
+        }
+
         Content = Id switch
         {
             1 => "前言",
             2 => "簡介",
             _ => string.Empty
         };
+
+        base.OnParametersSet();
     }
 }
