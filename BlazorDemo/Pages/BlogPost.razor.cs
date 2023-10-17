63a0e4aa9716fec7645835b0dfb6cc2eccbcbe06
Corvus <Corvus9312@gmail.com>
2023-10-17 11:00:17 +0800

    change-to-code-behind

---
+++ BlazorDemo/Pages/BlogPost.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemo.Pages;
+
+public class BlogPostBase : ComponentBase
+{
+    [Parameter]
+    public int Id { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        Content = Id switch
+        {
+            1 => "前言",
+            2 => "簡介",
+            _ => string.Empty
+        };
+    }
+}
